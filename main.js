3f25c7e77529c0044f776e3ffc7ddbf8618e9064
Allison Van Pelt <avantpelt@gmail.com>
2022-11-22 21:08:16 -0500

    Piano Audio
    - Use Web Audio to generate and play sound for each piano key while key is being pressed

--- main.js
+++ main.js
@@ -1,7 +1,63 @@
+const frequencies = {
+    "note-c": 261.63,
+    "note-c-sharp": 277.18,
+    "note-d": 293.66,
+    "note-d-sharp": 311.13,
+    "note-e": 329.63,
+    "note-f": 349.23,
+    "note-f-sharp": 369.99,
+    "note-g": 392.00,
+    "note-g-sharp": 415.30,
+    "note-a": 440.00,
+    "note-a-sharp": 466.16,
+    "note-b": 493.88
+};
+
+// Create an AudioContext
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+
+// Create a GainNode & initialize the volume level
+const volume = audioContext.createGain();
+volume.gain.value = 1;
+
+// Connect the GainNode to the audio output
+volume.connect(audioContext.destination);
+
 const keys = document.querySelectorAll(".key");
-keys.forEach(key => 
+keys.forEach(key => {
+    let oscillator = createOscillator(key.id);
+
     key.addEventListener(
-        "click", 
-        function() { console.log("Clicked: " + key.id) }
-    )
+        "mousedown",
+        function () {
+            // Play note
+            oscillator.start();
+        }
+    );
+
+    key.addEventListener(
+        "click",
+        function () {
+            // Stop note
+            oscillator.stop();
+
+            // Create a new OscillatorNode for this note
+            // An OscillatorNode cannot be re-started after being stopped.
+            oscillator = createOscillator(key.id);
+        }
+    );
+}
 );
+
+function createOscillator(note) {
+    // Create an OscillatorNode & initialize the waveform
+    const oscillator = new OscillatorNode(audioContext);
+    oscillator.type = 'sine';
+    oscillator.frequency.value = frequencies[note];
+
+    // Connect the OscillatorNode (audio source) to the GainNode (audio effect)
+    oscillator.connect(volume);
+
+    return oscillator;
+}
